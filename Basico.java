5f4502aaf5781ae7e82fc1d545fb4631ebb92c8f
Davi Souza de Oliveira <vlogsplayer10@gmail.com>
2022-08-08 21:56:39 -0300

    Basico.java
    Minha documentação sobre o java.

---
+++ Basico.java
@@ -0,0 +1,43 @@
+/*
+No README, vc entendeu o mínimo do Java, agora iremos entrar na linguagem junto com a OO. 
+
+nota: sempre que eu usar o termo OO; saiba que significa: Orientação a objetos.
+
+Se vc é programou uma única vez na vida, provavelmente sabe o que é um "Hello World" (significa literalmente: "Olá mundo"),
+caso não saiba, vai saber :D, confia.
+
+Em java, para exibir essa mensagem seria mais ou menos assim ↓↓↓↓. 
+*/
+System.out.print("Hello World");
+
+/*
+Explicando o código acima:
+
+System - É uma classe padrão do java que oferece os fluxos de entrada padrão, saída padrão, e saída de erro da linguagem. Você não precisa entender
+         isso inicialmente, apenas entenda o funcionamento, mais pra frente, abordarei o conceito de Classes.
+         
+out - Método de saída padrão da classe System. Também entenderemos o que é um método mais à frente.
+
+print() - método de exibição de valores; há outras variações desse método: println, prinf, explicarei-as depois.
+
+E por fim, o bendito ";" no final de toda instrução java é necessário colocá-lo, pois isso indica o fim da instrução, caso não seja colocado, resultará em
+erro de compilação.
+
+Apesar de tudo, mesmo que vc copie o trecho de código acima, ainda sim dará erro, java é uma linguagem burocrática, para que isso desse certo teria que ser
+algo assim ↓↓
+*/
+Public class Basico {
+  public static void main (String[] args) {
+    System.out.print("Hello World");
+  }
+}
+
+/*
+Explicando o código acima:
+
+Temos a nossa clase, denominada "Basico" 
+
+Bateu preguiça depois eu volto aqui;
+
+ADM a mimir........😴
+*/
